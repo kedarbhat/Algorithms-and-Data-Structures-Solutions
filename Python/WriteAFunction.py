acb4c73908265670f7677bb6b85a8de1f63c305c
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2019-01-10 19:57:11 -0600

    WriteAFunction answer

---
+++ Python/WriteAFunction.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+
+def is_leap(year: int) -> str:
+    return f'{"True"}' if ((year % 4 == 0) and ((year % 100 != 0) or (year % 400 == 0))) else f'{"False"}'
+
+if __name__ == '__main__':
+    print(is_leap(int(input)))
