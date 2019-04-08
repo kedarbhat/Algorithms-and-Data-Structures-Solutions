4acf39cb207b69903c5efca4d83b59851744dd82
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2019-04-08 00:06:10 -0500

    Added InsertNodeAtPosition solution/test

---
+++ hackerrank_cpp/src/LinkedListSolutions/InsertNodeAtPosition.hpp
@@ -0,0 +1,42 @@
+/*
+Copyright (C) 2019 Kedar R. Bhat
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem
+
+#pragma once
+
+#include "detail/SinglyLinkedListNode.hpp"
+
+namespace linked_lists {
+detail::SinglyLinkedListNode* insertNodeAtPosition(
+    detail::SinglyLinkedListNode* head, int data, int position) {
+  int counter = 0;
+  auto* node = head;
+  while (node != nullptr) {
+    if (position == counter + 1) {
+      auto* tmp = node->next_;
+      node->next_ = new detail::SinglyLinkedListNode(data);
+      node->next_->next_ = tmp;
+      break;
+    } else {
+      node = node->next_;
+      ++counter;
+    }
+  }
+  return head;
+}
+}  // namespace linked_lists
--- hackerrank_cpp/test/TestLinkedListSolutions.hpp
+++ hackerrank_cpp/test/TestLinkedListSolutions.hpp
@@ -18,23 +18,23 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #pragma once
 
 #include "LinkedListSolutions/InsertNodeAtHead.hpp"
+#include "LinkedListSolutions/InsertNodeAtPosition.hpp"
 #include "LinkedListSolutions/InsertNodeAtTail.hpp"
 #include "LinkedListSolutions/PrintLinkedListElements.hpp"
 
 namespace {
-  void DeleteLinkedList(linked_lists::detail::SinglyLinkedListNode* head) {
-    while (head != nullptr) {
-      auto* tmp = head->next_;
-      delete head;
-      head = tmp;
-    }
+void DeleteLinkedList(linked_lists::detail::SinglyLinkedListNode *head) {
+  while (head != nullptr) {
+    auto *tmp = head->next_;
+    delete head;
+    head = tmp;
   }
-} // namespace
+}
+}  // namespace
 
 namespace {
 TEST(TestLinkedListSolutions, PrintListedListElements) {
-  auto *head =
-      new linked_lists::detail::SinglyLinkedListNode(16);
+  auto *head = new linked_lists::detail::SinglyLinkedListNode(16);
   head->next_ = new linked_lists::detail::SinglyLinkedListNode(13);
   ASSERT_EQ(linked_lists::PrintLinkedList(head), std::string("16 13"));
   DeleteLinkedList(head);
@@ -42,13 +42,14 @@ TEST(TestLinkedListSolutions, PrintListedListElements) {
 
 TEST(TestLinkedListSolutions, InsertNodeAtTail) {
   linked_lists::detail::SinglyLinkedListNode *head = nullptr;
-  for (auto i : {141, 302,  164, 530, 474}) {
+  for (auto i : {141, 302, 164, 530, 474}) {
     head = linked_lists::InsertNodeAtTail(head, i);
   }
 
   for (auto i : {141, 302, 164, 530, 474}) {
     ASSERT_FALSE(head == nullptr);
-    ASSERT_EQ(head->data_, i); // NOLINT(clang-analyzer-core.NonNullParamChecker)
+    ASSERT_EQ(head->data_,
+              i);  // NOLINT(clang-analyzer-core.NonNullParamChecker)
     head = head->next_;
   }
   DeleteLinkedList(head);
@@ -62,7 +63,25 @@ TEST(TestLinkedListSolutions, InsertNodeAtHead) {
 
   for (auto i : {321, 975, 392, 484, 383}) {
     ASSERT_FALSE(head == nullptr);
-    ASSERT_EQ(head->data_, i); // NOLINT(clang-analyzer-core.NonNullParamChecker)
+    ASSERT_EQ(head->data_,
+              i);  // NOLINT(clang-analyzer-core.NonNullParamChecker)
+    head = head->next_;
+  }
+  DeleteLinkedList(head);
+}
+
+TEST(TestLinkedListSolutions, InsertNodeAtPosition) {
+  linked_lists::detail::SinglyLinkedListNode *head = nullptr;
+  for (auto i : {16, 13, 7}) {
+    head = linked_lists::InsertNodeAtTail(head, i);
+  }
+
+  head = linked_lists::insertNodeAtPosition(head, 1, 2);
+
+  for (auto i : {16, 13, 1, 7}) {
+    ASSERT_FALSE(head == nullptr);
+    ASSERT_EQ(head->data_,
+              i);  // NOLINT(clang-analyzer-core.NonNullParamChecker)
     head = head->next_;
   }
   DeleteLinkedList(head);
